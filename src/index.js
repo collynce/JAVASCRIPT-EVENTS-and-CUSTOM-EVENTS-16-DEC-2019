155a6ba0560d87ebf528692dc95f428f774d2052
Collins Kiprotich <collinsrono02@gmail.com>
2019-12-16 23:50:58 +0300

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -13,3 +13,32 @@ function showText() {
     }
     return updateText();
 }
+
+//Custom Events
+function myCustomEvent() {
+    let elem;
+    let event = new Event("event");
+    document.getElementById('custom_event').dispatchEvent(event);
+    document.getElementById('custom_event').addEventListener('event', function(){
+        console.log('This is my custom event...!!');
+    });
+}
+myCustomEvent();
+
+//Dynamic Typing
+function dataTyping() {
+    const x = true;
+    if (x){
+        console.log('This is a true event');
+
+    }else {
+        console.log('This is a false event');
+    }
+
+    let i=0;
+    for (i; i<10; i++){
+        console.log('This is a loop')
+    }
+
+}
+dataTyping();
